fec073c6a3f2696702f4c8a4617f01e4a737d5f3
EVE <953155109@qq.com>
2024-04-27 21:50:37 +0800

    fix:提交

--- src/views/pay/NavBar.vue
+++ src/views/pay/NavBar.vue
@@ -4,16 +4,16 @@
  * @Author: EVE
  * @Date: 2024-04-20 21:47:19
  * @LastEditors: EVE
- * @LastEditTime: 2024-04-21 10:26:47
+ * @LastEditTime: 2024-04-27 21:50:18
 -->
 <template>
   <van-nav-bar @click-left="router.back">
     <template #title>
       {{ getTitle }}
     </template>
-    <!-- <template #left>
+    <template #left>
       <i class="i-ic:sharp-arrow-back-ios" text-xl />
-    </template> -->
+    </template>
     <template #right>
       <slot name="right" />
     </template>
